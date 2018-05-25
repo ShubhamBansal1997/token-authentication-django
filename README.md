683187ce0c82bdd3cb4c98846beee039f43bd8e1
Shubham Bansal <shubhambansal17@hotmail.com>
2018-05-26 02:03:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Token-Based-Authentication-django
+This repo contains the code used to in the tutorial on the [medium blog](https://medium.com/@shubhambansal_89125/crud-app-using-vue-js-and-django-516edf4e4217)
+
+# Getting Started
+
+```sh
+$ git clone https://github.com/ShubhamBansal1997/token-authentication-django.git
+$ cd token-authentication-django/
+$ virtualenv --python=`which python3` venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+$ cd myproject
+$ python manage.py migrate
+$ python manage.py runserver
+```
+Application is started at 
+```sh
+127.0.0.1:8000
+```
+
+License
+----
+
+MIT
+
+***
+
+Contributors
+---
+If you would any error or issue you can mention in issues or raise a pull request.
+
+Made with ❤
+
---
+++ myproject/myproject/views.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+from rest_framework.response import Response
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'},
+                        status=HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key},
+                    status=HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(["GET"])
+def sample_api(request):
+    data = {'sample_data': 123}
+    return Response(data, status=HTTP_200_OK)
